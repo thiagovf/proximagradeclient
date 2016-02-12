3a4f9d63d7f2d1e899d2d1a16f85c8f9a9662b0b
thiagovf <thiagu.vf@gmail.com>
2016-02-12 16:24:38 -0300

    test

---
+++ src/br/com/k19/sessionbeans/Carrinho.java
@@ -0,0 +1,10 @@
+package br.com.k19.sessionbeans;
+
+import java.util.Set;
+
+public interface Carrinho {
+	void adiciona(String produto);
+	void remove(String produto);
+	Set<String> getProdutos();
+	void finalizaCompra();
+}
